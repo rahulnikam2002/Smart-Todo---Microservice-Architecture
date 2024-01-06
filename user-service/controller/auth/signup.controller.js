3b15cf8e594737f2f3606fb0607dfb052d268613
Rahul Nikam <codewithrahulnikam@gmail.com>
2024-01-06 16:44:00 +0530

    Feat: addded helper function to get value from redis

--- user-service/controller/auth/signup.controller.js
+++ user-service/controller/auth/signup.controller.js
@@ -1,3 +1,19 @@
+/**
+ *
+ * @param {*} req
+ * @param {*} res
+ *
+ *  is user aleady present? in Cache and then in database
+ *  if no! -> register
+ *  else -> res
+ * 
+ */
+
+const checkInRedis = require("../../utils/redis/checkInRedis");
+
 exports.signUp = async (req, res) => {
-  res.send("Sign up working fine");
+  const { userEmail } = req.body;
+
+  const a = await checkInRedis(userEmail);
+  return res.send(a);
 };
---
+++ user-service/utils/redis/checkInRedis.js
@@ -0,0 +1,29 @@
+const redisClient = require("../../lib/redis/redis.connect");
+
+const checkInRedis = async (key) => {
+  try {
+    if (!key) {
+      throw {
+        error: "Redis key is required",
+        isError: true,
+        value: null
+      };
+    }
+
+    const value = await redisClient.get(key);
+
+    return {
+      error: null,
+      value,
+      isError: false
+    };
+  } catch (error) {
+    return {
+      error: error.error,
+      isError: true,
+      value: null
+    };
+  }
+};
+
+module.exports = checkInRedis;
